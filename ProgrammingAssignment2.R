ee2c00d50085db888c722101cea76888728d07cd
Bas-Biesbroeck <basbiesbroeck@gmail.com>
2020-08-05 11:28:24 +0200

    Assignment 2, cached inversed matrix

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,25 @@
+makeCacheMatrix <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) m <<- inverse
+    getInverse <- function() m
+    list(set = set, get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
+}
+
+cacheSolve <- function(x, ...) {
+    m <- x$getInverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    mat <- x$get()
+    m <- solve(data, ...)
+    x$setInverse(m)
+    m
+}
